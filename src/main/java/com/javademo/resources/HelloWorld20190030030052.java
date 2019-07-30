9fe1f033fe69c3299ec69feab7214cb24bf93220
hjrocha <hjrocha@192.168.1.4>
2019-07-30 15:00:52 +0100

    Added HelloWorld20190030030052.java

---
+++ src/main/java/com/javademo/resources/HelloWorld20190030030052.java
@@ -0,0 +1,32 @@
+package com.javademo.resources;
+
+import com.google.common.base.Optional;
+import com.codahale.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Path("/hello-world-20190030030052")
+@Produces(MediaType.APPLICATION_JSON)
+public class HelloWorld20190030030052 {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+
+    public HelloWorld20190030030052(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Saying sayHello(@QueryParam("name") Optional<String> name) {
+        final String value = String.format(template, name.or(defaultName));
+        return new Saying(counter.incrementAndGet(), value);
+    }
+}
