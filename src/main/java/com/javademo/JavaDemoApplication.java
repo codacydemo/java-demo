3786618debfb81259410a678638395d9e17d22e9
Helio Rocha <hjrocha@gmail.com>
2021-05-19 14:09:55 +0100

    adding a comment

--- src/main/java/com/javademo/JavaDemoApplication.java
+++ src/main/java/com/javademo/JavaDemoApplication.java
@@ -12,7 +12,7 @@ public class JavaDemoApplication extends Application<JavaDemoConfiguration> {
 
     @Override
     public String getName() {
-        //TODO: Just a change
+        //TODO: Just a change to test a commit info
         return "java-demo";
     }
 
